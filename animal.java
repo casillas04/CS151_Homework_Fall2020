efc5dd8799936fcac90e3988449c50aa6fc70c4e
casillas04 <lisiyuan04@gmail.com>
2020-09-10 19:46:15 -0700

    Add files via upload
    This one is the demo for our homework

---
+++ animal.java
@@ -0,0 +1,13 @@
+package cs151_homework1;
+
+public class animal {
+    public static void main(String []args) {
+    	String s="";
+    	String s1=s+" "+"/"+"\\"+"_"+"/"+"\\"+"    "+"-----";
+    	String s2=s+"("+" "+"'"+" "+"'"+" "+")"+" <"+"/"+"Hello"+"\\";
+    	String s3=s+" "+"-----";
+    	System.out.println(s1);
+    	System.out.println(s2);
+    	System.out.println(s3);
+    }
+}
